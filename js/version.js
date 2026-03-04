1de77dce1816720fc821ce066fc6ce1c9014494c
marston-gould <gould.marston@gmail.com>
2026-03-04 16:56:55 -0500

    v7.06 — Referral Outreach Templates (Part 1)

--- js/version.js
+++ js/version.js
@@ -1,4 +1,4 @@
-var BJ_VERSION = 'v7.05';
+var BJ_VERSION = 'v7.06';
 (function() {
   function populateVersion() {
     document.querySelectorAll(".bj-version, [id$=\"-version\"]").forEach(function(el) {
@@ -11,5 +11,6 @@ var BJ_VERSION = 'v7.05';
     populateVersion();
   }
 })();
+
 
 
