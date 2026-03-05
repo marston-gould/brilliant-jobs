bf54bc51ad69b16a575b04850eadc93d7a32a409
marston-gould <gould.marston@gmail.com>
2026-03-04 20:29:51 -0500

    v7.13: remove wfts, count:planned, fix feed timeouts

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-// [BJ] Dashboard v7.12 loaded
-console.log('[BJ] Dashboard v7.12 loaded');
+// [BJ] Dashboard v7.13 loaded
+console.log('[BJ] Dashboard v7.13 loaded');
 // BJ_VERSION is defined in js/version.js (single source of truth)
 // version.js auto-populates #nav-version and .bj-version elements
 
